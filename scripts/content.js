1af8c093c82a98621d24da449e3aa670010f1081
Menno Veerkamp <veerkampmenno@gmail.com>
2024-07-19 17:24:24 +0200

    fix(scripts): add IIFE for already declared variables

--- scripts/content.js
+++ scripts/content.js
@@ -1,26 +1,28 @@
-html = document.documentElement;
-urlObject = new URL(location.href);
-domain = urlObject.origin.replace("www.", "");
+(() => {
+    html = document.documentElement;
+    urlObject = new URL(location.href);
+    domain = urlObject.origin.replace("www.", "");
 
-const setGlobalBrightness = async () => {
-    const result = await chrome.storage.local.get("globalBrightness");
-    const globalBrightness = result.globalBrightness;
-    html.style.setProperty("filter", `brightness(${globalBrightness / 100})`, "important");
-};
+    const setGlobalBrightness = async () => {
+        const result = await chrome.storage.local.get("globalBrightness");
+        const globalBrightness = result.globalBrightness;
+        html.style.setProperty("filter", `brightness(${globalBrightness / 100})`, "important");
+    };
 
-const setLocalBrightness = async () => {
-    const result = await chrome.storage.local.get("knownWebsites");
-    if (result.knownWebsites?.length) {
-        let index = result.knownWebsites.findIndex((obj) => obj.domain === domain);
+    const setLocalBrightness = async () => {
+        const result = await chrome.storage.local.get("knownWebsites");
+        if (result.knownWebsites?.length) {
+            let index = result.knownWebsites.findIndex((obj) => obj.domain === domain);
 
-        if (index != -1) {
-            let brightness = result.knownWebsites[index].brightness;
+            if (index != -1) {
+                let brightness = result.knownWebsites[index].brightness;
 
-            html.style.setProperty("filter", `brightness(${brightness / 100})`, "important");
-            return;
+                html.style.setProperty("filter", `brightness(${brightness / 100})`, "important");
+                return;
+            }
         }
-    }
-    setGlobalBrightness();
-};
+        setGlobalBrightness();
+    };
 
-setLocalBrightness();
+    setLocalBrightness();
+})()
